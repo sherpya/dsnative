a4ca71030f0d48ce005dc6c8310a2413d059b388
sherpya <sherpya@netfarm.it>
2010-01-27 01:24:19 +0100

    cosmetics

--- dsnative.cpp
+++ dsnative.cpp
@@ -278,30 +278,18 @@ public:
 
     HRESULT AddToRot(IUnknown *pUnkGraph, DWORD *pdwRegister) 
     {
-        IMoniker * pMoniker = NULL;
+        IMoniker *pMoniker = NULL;
         IRunningObjectTable *pROT = NULL;
 
-        if (FAILED(GetRunningObjectTable(0, &pROT))) 
-        {
+        if (FAILED(GetRunningObjectTable(0, &pROT)))
             return E_FAIL;
-        }
 
-        const size_t STRING_LENGTH = 256;
-
-        WCHAR wsz[STRING_LENGTH];
-
-        StringCchPrintfW(
-            wsz, STRING_LENGTH, 
-            L"FilterGraph %08x pid %08x", 
-            (DWORD_PTR)pUnkGraph, 
-            GetCurrentProcessId()
-            );
-
+        WCHAR wsz[256];
+        StringCchPrintfW(wsz, 256, L"FilterGraph %08x pid %08x", (DWORD_PTR) pUnkGraph, GetCurrentProcessId());
         HRESULT hr = CreateItemMoniker(L"!", wsz, &pMoniker);
-        if (SUCCEEDED(hr)) 
+        if (SUCCEEDED(hr))
         {
-            hr = pROT->Register(ROTFLAGS_REGISTRATIONKEEPSALIVE, pUnkGraph,
-                pMoniker, pdwRegister);
+            hr = pROT->Register(ROTFLAGS_REGISTRATIONKEEPSALIVE, pUnkGraph, pMoniker, pdwRegister);
             pMoniker->Release();
         }
         pROT->Release();
