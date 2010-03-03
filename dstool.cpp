b9eaebdd8cfc8ee1ec4e614916f6545c571864b1
sherpya <sherpya@netfarm.it>
2010-03-03 16:35:07 +0100

    added basic sample for dstool

--- dstool.cpp
+++ dstool.cpp
@@ -21,34 +21,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _DSCodec DSCodec;
-typedef DSCodec * (WINAPI *funcDSOpenCodec)(const char *dll, const GUID guid, BITMAPINFOHEADER* bih);
-typedef void (WINAPI *funcDSCloseCodec)(DSCodec *codec);
-typedef BOOL (WINAPI *funcDSShowPropertyPage)(DSCodec *codec);
+#define DSN_OK 0
+#define DSN_API_VERSION 4
 
-//const GUID guid = { 0x09571a4b, 0xf1fe, 0x4c60, { 0x97, 0x60, 0xde, 0x6d, 0x31, 0x0c, 0x7c, 0x31 } };
-//const char codec[] = "CoreAVCDecoder.ax";
-//const GUID guid = { 0x82CCD3E0, 0xF71A, 0x11D0, { 0x9F, 0xE5, 0x00, 0x60, 0x97, 0x78, 0xEA, 0x66 }};
-//const char codec[] = "mpg4ds32.ax";
+typedef struct _DSVideoCodec DSVideoCodec;
+typedef DSVideoCodec * (WINAPI *funcDSOpenVideoCodec)(const char *dll, const GUID guid, BITMAPINFOHEADER* bih,
+                                                        unsigned int outfmt, float fps, const char *filename, int *err);
+typedef void (WINAPI *funcDSCloseVideoCodec)(DSVideoCodec *codec);
+typedef int (WINAPI *funcDSVideoDecode)(DSVideoCodec *vcodec, const BYTE *src, int size, double pts,
+                                          double *newpts, BYTE *pImage, int keyframe);
+typedef int (WINAPI *funcDSVideoResync)(DSVideoCodec *codec, double pts);
+typedef const char * (WINAPI *funcDSStrError)(int error);
+typedef unsigned int (WINAPI *funcDSGetApiVersion)(void);
 
-const GUID guid = { 0x521fb373, 0x7654, 0x49f2, { 0xbd, 0xb1, 0x0c, 0x6e, 0x66, 0x60, 0x71, 0x4f } };
-const char codec[] = "wmv8ds32.ax";
-
-//const GUID guid = { 0x4cb63e61, 0xc611, 0x11D0, { 0x83, 0xaa, 0x00, 0x00, 0x92, 0x90, 0x01, 0x84 } };
-//const char codec[] = "tm20dec.ax";
-
-//const GUID guid = { 0x5DFA20A4, 0xC343, 0x45DF, { 0xA4, 0x6E, 0xEC, 0x17, 0x11, 0xE4, 0xEE, 0x32 } };
-//const char codec[] = "diavc.ax";
+static struct
+{
+    char *filename;
+    GUID guid;
+} codecs[] = { "ffdshow.ax", { 0x04fe9017, 0xf873, 0x410e, { 0x87, 0x1e, 0xab, 0x91, 0x66, 0x1a, 0x4e, 0xf7 } } };
 
 int main(void)
 {
     HMODULE hDs = LoadLibrary("dsnative.dll");
-    funcDSOpenCodec DSOpenCodec = (funcDSOpenCodec) GetProcAddress(hDs, "DSOpenCodec");
-    funcDSCloseCodec DSCloseCodec = (funcDSCloseCodec) GetProcAddress(hDs, "DSCloseCodec");
-    funcDSShowPropertyPage DSShowPropertyPage = (funcDSShowPropertyPage) GetProcAddress(hDs, "DSShowPropertyPage");
-    DSCodec *c = DSOpenCodec(codec, guid, NULL);
-    //__asm { int 3 };
-    //DSShowPropertyPage(c);
-    DSCloseCodec(c);
+    funcDSOpenVideoCodec DSOpenVideoCodec = (funcDSOpenVideoCodec) GetProcAddress(hDs, "DSOpenVideoCodec");
+    funcDSCloseVideoCodec DSCloseVideoCodec = (funcDSCloseVideoCodec) GetProcAddress(hDs, "DSCloseVideoCodec");
+
+    BITMAPINFOHEADER bi;
+    memset(&bi, 0, sizeof(bi));
+    bi.biSize = sizeof(bi);
+    bi.biCompression = mmioFOURCC('X', 'V', 'I', 'D');
+    bi.biWidth = 320;
+    bi.biHeight = 240;
+
+    DSVideoCodec *c = DSOpenVideoCodec(codecs[0].filename, codecs[0].guid, &bi, mmioFOURCC('Y', 'V', '1', '2'), 25.0f, NULL, NULL);
+    if (c) DSCloseVideoCodec(c);
     return 0;
 }
